b215a9f8d15d28a85e76185cc33feb278f735db0
Panos Paganis <mallinanga@gmail.com>
2015-07-12 11:26:13 +0300

    Require dependencies

--- nanga-deploy.php
+++ nanga-deploy.php
@@ -9,6 +9,18 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
+add_action( 'tgmpa_register', function () {
+    $plugins = array(
+        array(
+            'name'               => 'WP Migrate DB',
+            'slug'               => 'wp-migrate-db',
+            'required'           => true,
+            'force_activation'   => true,
+            'force_deactivation' => false
+        ),
+    );
+    tgmpa( $plugins );
+} );
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
     include 'nanga-command.php';
 }
